8e48392437ad2aed1b23bdb48efce588bf80ac87
Umi <umy.ikem@gmail.com>
2021-03-01 22:05:25 +0100

    update emulator name constant

--- Carbon-Automation/src/main/java/org/carbon/base.java
+++ Carbon-Automation/src/main/java/org/carbon/base.java
@@ -11,13 +11,13 @@ import java.net.URL;
 
 public class base {
 
-    private static final String emulatorName = "umi-emulator";
+    private static final String EMULATOR_NAME = "umi-emulator";
 
     public static AndroidDriver<AndroidElement> Capabilities(String apk) throws MalformedURLException {
         File appDir = new File("src");
         File app = new File(appDir, apk);
         DesiredCapabilities cap = new DesiredCapabilities();
-        cap.setCapability(MobileCapabilityType.DEVICE_NAME, emulatorName );
+        cap.setCapability(MobileCapabilityType.DEVICE_NAME, EMULATOR_NAME );
         cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
         cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
         AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"),cap);
